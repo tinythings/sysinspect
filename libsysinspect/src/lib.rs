e80b26909aecaff79fed20dda3fe746f9639f958
Bo Maryniuk <bo@maryniuk.net>
2024-09-20 15:28:43 +0200

    Add custom errors

--- libsysinspect/src/lib.rs
+++ libsysinspect/src/lib.rs
@@ -1,2 +1,26 @@
+use std::{
+    error::Error,
+    fmt::{Display, Formatter, Result},
+};
+
+pub mod mdl;
 pub mod runtime;
 pub mod tpl;
+
+#[derive(Debug)]
+pub enum SyspectError {
+    ModelMultipleIndex,
+}
+
+impl Error for SyspectError {}
+
+impl Display for SyspectError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let msg = match self {
+            SyspectError::ModelMultipleIndex => "Multiple model.cfg found at this path",
+        };
+
+        write!(f, "ERROR: {msg}")?;
+        Ok(())
+    }
+}
