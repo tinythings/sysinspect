0dff151d5b61d0e220fef22d29b8f5ed7af0d28d
Bo Maryniuk <bo@maryniuk.net>
2024-09-20 16:18:05 +0200

    Expand custom errors

--- libsysinspect/src/lib.rs
+++ libsysinspect/src/lib.rs
@@ -1,7 +1,10 @@
 use std::{
     error::Error,
     fmt::{Display, Formatter, Result},
+    io,
 };
+
+use mdl::mspec;
 
 pub mod mdl;
 pub mod runtime;
@@ -9,18 +12,35 @@ pub mod tpl;
 
 #[derive(Debug)]
 pub enum SyspectError {
-    ModelMultipleIndex,
+    ModelMultipleIndex(String),
+    IoErr(io::Error),
 }
 
-impl Error for SyspectError {}
+impl Error for SyspectError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            SyspectError::IoErr(err) => Some(err),
+            _ => None,
+        }
+    }
+}
 
 impl Display for SyspectError {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         let msg = match self {
-            SyspectError::ModelMultipleIndex => "Multiple model.cfg found at this path",
+            SyspectError::ModelMultipleIndex(m) => {
+                format!("Another {} file found as '{}'", mspec::MODEL_INDEX, m)
+            }
+            SyspectError::IoErr(err) => format!("(I/O) {err}"),
         };
 
-        write!(f, "ERROR: {msg}")?;
+        write!(f, "{msg}")?;
         Ok(())
+    }
+}
+
+impl From<io::Error> for SyspectError {
+    fn from(err: io::Error) -> Self {
+        SyspectError::IoErr(err)
     }
 }
