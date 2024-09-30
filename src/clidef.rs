904f0003a113f85e95fc7a03a3d4524686397318
Bo Maryniuk <bo@maryniuk.net>
2024-09-30 20:13:28 +0200

    Add CLI options

---
+++ src/clidef.rs
@@ -0,0 +1,58 @@
+use clap::builder::styling;
+use clap::{Arg, ArgAction, Command};
+use colored::Colorize;
+
+static APPNAME: &str = "sysinspect";
+
+/// Define CLI arguments and styling
+pub fn cli(version: &'static str) -> Command {
+    let styles = styling::Styles::styled()
+        .header(styling::AnsiColor::White.on_default() | styling::Effects::BOLD)
+        .usage(styling::AnsiColor::White.on_default() | styling::Effects::BOLD)
+        .literal(styling::AnsiColor::BrightCyan.on_default())
+        .placeholder(styling::AnsiColor::Cyan.on_default());
+
+    Command::new(APPNAME)
+        .version(version)
+        .about(format!("{} - {}", APPNAME, "is a tool for anomaly detection and root cause analysis in a known system"))
+        .override_usage(format!("{} {} {}", APPNAME.bright_cyan(), "[OPTIONS]".cyan(), "[FILTERS]".cyan()))
+
+        // Config
+        .arg(
+            Arg::new("model")
+                .short('m')
+                .long("model")
+                .help("System description model")
+        )
+
+        // Other
+        .next_help_heading("Other")
+        .arg(
+            Arg::new("debug")
+                .short('d')
+                .long("debug")
+                .action(ArgAction::SetTrue)
+                .help("Set debug mode for more verbose output."),
+        )
+        .arg(
+            Arg::new("help")
+                .short('h')
+                .long("help")
+                .action(ArgAction::SetTrue)
+                .help("Display help"),
+        )
+        .arg(
+            Arg::new("version")
+                .short('v')
+                .long("version")
+                .action(ArgAction::SetTrue)
+                .help("Get current version."),
+        )
+        .disable_help_flag(true) // Otherwise it is displayed in a wrong position
+        .disable_version_flag(true)
+        .disable_colored_help(false)
+        .styles(styles)
+        .after_help("NOTE: This tool is in very early development.
+      If it doesn't work for you, please fill a bug report here:
+      https://github.com/tinythings/sysinspect/issues\n".bright_yellow().to_string())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
+use std::env;
+
+mod clidef;
+mod logger;
 mod mcf;
 
+static VERSION: &str = "0.1";
+static LOGGER: logger::STDOUTLogger = logger::STDOUTLogger;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut cli = clidef::cli(&VERSION);
+
+    if args.len() == 1 {
+        return {
+            cli.print_help().unwrap();
+        };
+    }
+
+    // Our main params
+    let params = cli.to_owned().get_matches();
+
+    // Print help?
+    if *params.get_one::<bool>("help").unwrap() {
+        return {
+            cli.print_help().unwrap();
+        };
+    }
+
+    // Print version?
+    if *params.get_one::<bool>("version").unwrap() {
+        return {
+            println!("Version {}", VERSION);
+        };
+    }
+
+    // Setup logger
+    if let Err(err) = log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(if params.get_flag("debug") { log::LevelFilter::Trace } else { log::LevelFilter::Info }))
+    {
+        return println!("{}", err.to_string());
+    }
+
+    /*
+    let r = match libsysinspect::mdl::mspec::load(".") {
+        Ok(m) => {
+            println!("{:?}", m);
+        }
+        Err(err) => println!("Error: {}", err),
+    };
+    */
 }
