66ecaafe02d1934078b50c5c27916bf0d2ec7bc3
Bo Maryniuk <bo@maryniuk.net>
2024-10-02 11:38:34 +0200

    Add actions scaffold

---
+++ libsysinspect/src/intp/actions.rs
@@ -0,0 +1,13 @@
+pub struct Action {}
+
+impl Action {
+    pub fn new() -> Self {
+        Action {}
+    }
+}
+
+impl Default for Action {
+    fn default() -> Self {
+        Action::new()
+    }
+}
