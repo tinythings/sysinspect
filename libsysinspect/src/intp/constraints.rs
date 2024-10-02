9e63205779f96fe3db2fe277672a2d2c55efc275
Bo Maryniuk <bo@maryniuk.net>
2024-10-02 11:38:52 +0200

    Add constraints scaffold

---
+++ libsysinspect/src/intp/constraints.rs
@@ -0,0 +1,13 @@
+pub struct Constraint {}
+
+impl Constraint {
+    pub fn new() -> Self {
+        Constraint {}
+    }
+}
+
+impl Default for Constraint {
+    fn default() -> Self {
+        Constraint::new()
+    }
+}
