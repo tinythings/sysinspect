ef8e4ca67246c512cdd80dd679ce96a475f23723
Bo Maryniuk <bo@maryniuk.net>
2024-10-09 14:12:00 +0200

    Add type converters for the entity configuration

--- libsysinspect/src/intp/conf.rs
+++ libsysinspect/src/intp/conf.rs
@@ -4,10 +4,86 @@ use serde_yaml::Value;
 use std::{collections::HashMap, path::PathBuf};
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub struct EventConfigOption {
+    #[serde(flatten)]
+    data: HashMap<String, Value>,
+}
+
+impl EventConfigOption {
+    /// Get an option as a string type
+    pub fn as_string(&self, cfg: &str) -> Option<String> {
+        if let Some(v) = self.data.get(cfg) {
+            match v {
+                Value::String(v) => return Some(v.to_owned()),
+                _ => {}
+            }
+        }
+
+        None
+    }
+
+    /// Get an option as an integer
+    pub fn as_int(&self, cfg: &str) -> Option<i64> {
+        if let Some(v) = self.data.get(cfg) {
+            match v {
+                Value::Number(v) => {
+                    if let Some(v) = v.as_i64() {
+                        return Some(v);
+                    }
+                }
+                _ => {}
+            }
+        }
+        None
+    }
+
+    /// Get an option as an integer
+    pub fn as_bool(&self, cfg: &str) -> Option<bool> {
+        if let Some(v) = self.data.get(cfg) {
+            match v {
+                Value::Bool(v) => return Some(v.to_owned()),
+                _ => {}
+            }
+        }
+        None
+    }
+
+    /// Get an option as a vector of strings
+    pub fn as_str_list(&self, cfg: &str) -> Option<Vec<String>> {
+        if let Some(v) = self.data.get(cfg) {
+            match v {
+                Value::Sequence(v) => {
+                    let mut out: Vec<String> = Vec::default();
+                    for i in v {
+                        if let Some(i) = i.as_str() {
+                            out.push(i.to_string());
+                        }
+                    }
+                    return if v.len() == out.len() { Some(out) } else { None };
+                }
+                _ => {}
+            }
+        }
+        None
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct EventsConfig {
     handler: Vec<String>,
     #[serde(flatten)]
-    cfg: Option<HashMap<String, serde_yaml::Value>>,
+    cfg: Option<HashMap<String, EventConfigOption>>,
+}
+
+impl EventsConfig {
+    pub fn for_handler(&self, handler: &str) -> Option<EventConfigOption> {
+        if let Some(cfg) = &self.cfg {
+            if let Some(cfg) = cfg.get(handler) {
+                return Some(cfg.to_owned());
+            }
+        }
+        None
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
