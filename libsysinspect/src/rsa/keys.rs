68533f0960987f90f548d71fed0b621b641eefd3
Bo Maryniuk <bo@maryniuk.net>
2024-11-03 17:28:29 +0100

    Write keys to a file as PEM

--- libsysinspect/src/rsa/keys.rs
+++ libsysinspect/src/rsa/keys.rs
@@ -7,10 +7,16 @@ use rsa::{
     signature::{Keypair, SignatureEncoding, Verifier},
     Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey,
 };
-use std::error::Error;
+use std::{error::Error, fs, io, path::PathBuf};
+
+use crate::SysinspectError;
 
 /// Default key size.
 pub static DEFAULT_KEY_SIZE: usize = 1048;
+pub enum RsaKey {
+    Prk(RsaPrivateKey),
+    Pbk(RsaPublicKey),
+}
 
 /// Generate RSA keys
 pub fn keygen(bits: usize) -> Result<(RsaPrivateKey, RsaPublicKey), Box<dyn Error>> {
@@ -22,10 +28,20 @@ pub fn keygen(bits: usize) -> Result<(RsaPrivateKey, RsaPublicKey), Box<dyn Erro
 }
 
 /// Serializes RSA private and public keys to PEM format.
-pub fn to_pem(prk: &RsaPrivateKey, pbk: &RsaPublicKey) -> Result<(String, String), Box<dyn Error>> {
+pub fn to_pem(
+    prk: Option<&RsaPrivateKey>, pbk: Option<&RsaPublicKey>,
+) -> Result<(Option<String>, Option<String>), Box<dyn Error>> {
     Ok((
-        pem::encode(&pem::Pem::new("RSA PRIVATE KEY", prk.to_pkcs1_der()?.as_bytes().to_vec())),
-        pem::encode(&pem::Pem::new("RSA PUBLIC KEY", pbk.to_pkcs1_der()?.as_bytes().to_vec())),
+        if prk.is_some() {
+            Some(pem::encode(&pem::Pem::new("RSA PRIVATE KEY", prk.unwrap().to_pkcs1_der()?.as_bytes().to_vec())))
+        } else {
+            None
+        },
+        if pbk.is_some() {
+            Some(pem::encode(&pem::Pem::new("RSA PUBLIC KEY", pbk.unwrap().to_pkcs1_der()?.as_bytes().to_vec())))
+        } else {
+            None
+        },
     ))
 }
 
@@ -43,7 +59,7 @@ pub fn sign_data(prk: RsaPrivateKey, data: &[u8]) -> Result<Vec<u8>, Box<dyn Err
     let sig = sk.sign(data);
     sk.verifying_key().verify(data, &sig)?;
 
-    Ok((&*sig.to_bytes()).to_vec())
+    Ok((*sig.to_bytes()).to_vec())
 }
 
 /// Verify signature from the pubic key
@@ -52,6 +68,7 @@ pub fn verify_sign(pbk: &RsaPublicKey, data: &[u8], sig: Vec<u8>) -> Result<bool
 }
 
 /// Get fingerprint of a public key
+#[allow(clippy::format_collect)]
 pub fn get_fingerprint(pbk: &RsaPublicKey) -> Result<String, Box<dyn Error>> {
     let mut digest = Sha256::new();
     digest.update(pbk.to_pkcs1_der()?.as_bytes());
@@ -66,4 +83,41 @@ pub fn encrypt(pbk: RsaPublicKey, data: Vec<u8>) -> Result<Vec<u8>, Box<dyn Erro
 // Decrypt data
 pub fn decrypt(prk: RsaPrivateKey, cipher: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
     Ok(prk.decrypt(Pkcs1v15Encrypt, &cipher)?)
+}
+
+/// Write private key as a file
+pub fn key_to_file(prk: &RsaKey, p: &str, name: &str) -> Result<(), SysinspectError> {
+    let p = PathBuf::from(p).join(name);
+    if p.exists() {
+        return Err(SysinspectError::IoErr(io::Error::new(
+            io::ErrorKind::AlreadyExists,
+            format!("File {} already exists", p.to_str().unwrap_or_default()),
+        )));
+    }
+
+    let mut pem = String::default();
+    match prk {
+        RsaKey::Prk(prk) => {
+            if let Ok((prk_pem, _)) = to_pem(Some(prk), None) {
+                if let Some(prk_pem) = prk_pem {
+                    pem = prk_pem;
+                }
+            } else {
+                return Err(SysinspectError::IoErr(io::Error::new(io::ErrorKind::InvalidData, "Unable to create PEM key")));
+            }
+        }
+        RsaKey::Pbk(pbk) => {
+            if let Ok((_, pbk_pem)) = to_pem(None, Some(pbk)) {
+                if let Some(pbk_pem) = pbk_pem {
+                    pem = pbk_pem;
+                }
+            } else {
+                return Err(SysinspectError::IoErr(io::Error::new(io::ErrorKind::InvalidData, "Unable to create PEM key")));
+            }
+        }
+    }
+    fs::write(&p, pem.as_bytes())?;
+    log::debug!("Wrote PEM file as {}", p.to_str().unwrap_or_default());
+
+    Ok(())
 }
