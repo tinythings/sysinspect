ded53b7b9a5d22739e6d0e2443e61b9b9c9c7456
Bo Maryniuk <bo@maryniuk.net>
2024-11-03 16:45:44 +0100

    Refactor RSA functions

--- libsysinspect/src/rsa/keys.rs
+++ libsysinspect/src/rsa/keys.rs
@@ -1,6 +1,12 @@
 use rand::rngs::OsRng;
-use rsa::pkcs1::{DecodeRsaPrivateKey, DecodeRsaPublicKey, EncodeRsaPrivateKey, EncodeRsaPublicKey};
-use rsa::{RsaPrivateKey, RsaPublicKey};
+use rsa::{
+    pkcs1::{DecodeRsaPrivateKey, DecodeRsaPublicKey, EncodeRsaPrivateKey, EncodeRsaPublicKey},
+    pkcs1v15::{Signature, SigningKey, VerifyingKey},
+    sha2::{Digest, Sha256},
+    signature::SignerMut,
+    signature::{Keypair, SignatureEncoding, Verifier},
+    Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey,
+};
 use std::error::Error;
 
 /// Default key size.
@@ -9,52 +15,55 @@ pub static DEFAULT_KEY_SIZE: usize = 1048;
 /// Generate RSA keys
 pub fn keygen(bits: usize) -> Result<(RsaPrivateKey, RsaPublicKey), Box<dyn Error>> {
     let mut rng = OsRng;
-    let private_key = RsaPrivateKey::new(&mut rng, bits)?;
-    let public_key = RsaPublicKey::from(&private_key);
+    let prk = RsaPrivateKey::new(&mut rng, bits)?;
+    let pbk = RsaPublicKey::from(&prk);
 
-    Ok((private_key, public_key))
+    Ok((prk, pbk))
 }
 
 /// Serializes RSA private and public keys to PEM format.
-///
-/// # Arguments
-///
-/// * `private_key` - A reference to the RSA private key.
-/// * `public_key` - A reference to the RSA public key.
-///
-/// # Returns
-///
-/// A tuple containing the PEM-encoded private and public keys as strings.
-pub fn to_pem(private_key: &RsaPrivateKey, public_key: &RsaPublicKey) -> Result<(String, String), Box<dyn Error>> {
-    // Serialize private key to PKCS#1 DER
-    let private_der = private_key.to_pkcs1_der()?;
-    let private_pem = pem::encode(&pem::Pem::new("RSA PRIVATE KEY", private_der.as_bytes().to_vec()));
-
-    // Serialize public key to PKCS#1 DER
-    let public_der = public_key.to_pkcs1_der()?;
-    let public_pem = pem::encode(&pem::Pem::new("RSA PUBLIC KEY", public_der.as_bytes().to_vec()));
-
-    Ok((private_pem, public_pem))
+pub fn to_pem(prk: &RsaPrivateKey, pbk: &RsaPublicKey) -> Result<(String, String), Box<dyn Error>> {
+    Ok((
+        pem::encode(&pem::Pem::new("RSA PRIVATE KEY", prk.to_pkcs1_der()?.as_bytes().to_vec())),
+        pem::encode(&pem::Pem::new("RSA PUBLIC KEY", pbk.to_pkcs1_der()?.as_bytes().to_vec())),
+    ))
 }
 
 /// Deserializes RSA private and public keys from PEM format.
-///
-/// # Arguments
-///
-/// * `private_pem` - A string slice containing the PEM-encoded private key.
-/// * `public_pem` - A string slice containing the PEM-encoded public key.
-///
-/// # Returns
-///
-/// A tuple containing the deserialized RSA private and public keys.
-pub fn from_pem(private_pem: &str, public_pem: &str) -> Result<(RsaPrivateKey, RsaPublicKey), Box<dyn Error>> {
-    // Deserialize private key from PEM
-    let parsed_private_pem = pem::parse(private_pem)?;
-    let private_key = RsaPrivateKey::from_pkcs1_der(parsed_private_pem.contents())?;
+pub fn from_pem(prk_pem: &str, pbk_pem: &str) -> Result<(RsaPrivateKey, RsaPublicKey), Box<dyn Error>> {
+    Ok((
+        RsaPrivateKey::from_pkcs1_der(pem::parse(prk_pem)?.contents())?,
+        RsaPublicKey::from_pkcs1_der(pem::parse(pbk_pem)?.contents())?,
+    ))
+}
 
-    // Deserialize public key from PEM
-    let parsed_public_pem = pem::parse(public_pem)?;
-    let public_key = RsaPublicKey::from_pkcs1_der(parsed_public_pem.contents())?;
+/// Sign data with the private key
+pub fn sign_data(prk: RsaPrivateKey, data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut sk = SigningKey::<Sha256>::new(prk);
+    let sig = sk.sign(data);
+    sk.verifying_key().verify(data, &sig)?;
 
-    Ok((private_key, public_key))
+    Ok((&*sig.to_bytes()).to_vec())
+}
+
+/// Verify signature from the pubic key
+pub fn verify_sign(pbk: &RsaPublicKey, data: &[u8], sig: Vec<u8>) -> Result<bool, Box<dyn Error>> {
+    Ok(VerifyingKey::<Sha256>::new(pbk.clone()).verify(data, &Signature::try_from(sig.as_slice())?).is_ok())
+}
+
+/// Get fingerprint of a public key
+pub fn get_fingerprint(pbk: &RsaPublicKey) -> Result<String, Box<dyn Error>> {
+    let mut digest = Sha256::new();
+    digest.update(pbk.to_pkcs1_der()?.as_bytes());
+    Ok(digest.finalize().iter().map(|byte| format!("{:02x}", byte)).collect())
+}
+
+// Encrypt data
+pub fn encrypt(pbk: RsaPublicKey, data: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
+    Ok(pbk.encrypt(&mut rand::thread_rng(), Pkcs1v15Encrypt, &data[..])?)
+}
+
+// Decrypt data
+pub fn decrypt(prk: RsaPrivateKey, cipher: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
+    Ok(prk.decrypt(Pkcs1v15Encrypt, &cipher)?)
 }
