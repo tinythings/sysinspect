5b922972e052e064fa14bde2f51a48c05d051064
Bo Maryniuk <bo@maryniuk.net>
2024-11-04 18:41:28 +0100

    Define request types

---
+++ libsysinspect/src/proto/rqtypes.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum RequestType {
+    /// Minion registration request or context.
+    #[serde(rename = "add")]
+    Add,
+
+    /// Minion un-registration request.
+    #[serde(rename = "rm")]
+    Remove,
+
+    /// Regular response to any Master command
+    #[serde(rename = "rsp")]
+    Response,
+
+    /// Regular command to any Minion
+    #[serde(rename = "cmd")]
+    Command,
+
+    /// Request to return all minion traits
+    #[serde(rename = "tr")]
+    Traits,
+
+    /// Hello/ehlo
+    #[serde(rename = "ehlo")]
+    Ehlo,
+}
