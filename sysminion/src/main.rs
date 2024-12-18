59f8f27d7dca910d3c018fb4ea7e675d8fea95c9
Bo Maryniuk <bo@maryniuk.net>
2024-12-18 20:50:30 +0100

    Narrow down tokio scope

--- sysminion/src/main.rs
+++ sysminion/src/main.rs
@@ -5,7 +5,7 @@ mod proto;
 mod rsa;
 
 use clidef::cli;
-use libsysinspect::logger;
+use libsysinspect::{logger, SysinspectError};
 use log::LevelFilter;
 use std::env;
 
@@ -13,8 +13,15 @@ static APPNAME: &str = "sysminion";
 static VERSION: &str = "0.3.0";
 static LOGGER: logger::STDOUTLogger = logger::STDOUTLogger;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
+fn start_minion(cfp: Option<&String>, fp: Option<String>) -> Result<(), SysinspectError> {
+    tokio::runtime::Runtime::new()?.block_on(async {
+        minion::minion(cfp.map_or("", |v| v), fp).await?;
+        Ok::<(), SysinspectError>(())
+    })?;
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
     let mut cli = cli(VERSION, APPNAME);
     if env::args().collect::<Vec<String>>().len() == 1 {
         cli.print_help()?;
@@ -24,12 +31,12 @@ async fn main() -> std::io::Result<()> {
     let params = cli.to_owned().get_matches();
 
     // Print help?
-    if *params.get_one::<bool>("help").unwrap() {
+    if params.get_flag("help") {
         return cli.print_help();
     }
 
     // Print version?
-    if *params.get_one::<bool>("version").unwrap() {
+    if params.get_flag("version") {
         println!("Version: {} {}", APPNAME, VERSION);
         return Ok(());
     }
@@ -48,10 +55,8 @@ async fn main() -> std::io::Result<()> {
     // Start
     let fp = params.get_one::<String>("register").cloned();
     if *params.get_one::<bool>("start").unwrap_or(&false) || fp.is_some() {
-        let cfp = params.get_one::<String>("config");
-        if let Err(err) = minion::minion(cfp.map_or("", |v| v), fp).await {
-            log::error!("Unable to start minion: {}", err);
-            return Ok(());
+        if let Err(err) = start_minion(params.get_one::<String>("config"), fp) {
+            log::error!("Error starting minion: {err}");
         }
     }
 
