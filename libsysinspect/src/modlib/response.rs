c4df3862faca5a6403d5032f502a828f59a00ede
Bo Maryniuk <bo@maryniuk.net>
2025-02-11 21:38:15 +0100

    Extend module response object for configuration management

--- libsysinspect/src/modlib/response.rs
+++ libsysinspect/src/modlib/response.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
-use serde_json::to_value;
+use serde_json::{json, to_value, Value};
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct ModResponse {
@@ -12,16 +14,16 @@ pub struct ModResponse {
     message: String,
 
     #[serde(skip_serializing_if = "is_json_null")]
-    data: serde_json::Value,
+    data: Value,
 }
 
 /// Skip data inclusion into serialised output if nothing is defined
-fn is_json_null(v: &serde_json::Value) -> bool {
+fn is_json_null(v: &Value) -> bool {
     match v {
-        serde_json::Value::Null => true,
-        serde_json::Value::Object(e) if e.is_empty() => true,
-        serde_json::Value::String(e) if e.is_empty() => true,
-        serde_json::Value::Array(e) if e.is_empty() => true,
+        Value::Null => true,
+        Value::Object(e) if e.is_empty() => true,
+        Value::String(e) if e.is_empty() => true,
+        Value::Array(e) if e.is_empty() => true,
         _ => false,
     }
 }
@@ -36,7 +38,18 @@ impl ModResponse {
     where
         T: Serialize,
     {
-        self.data = to_value(data)?;
+        //self.data = to_value(data)?;
+        let v = to_value(data)?;
+
+        if let Value::Object(ref mut map) = self.data {
+            if let Value::Object(n_map) = v {
+                for (k, v) in n_map {
+                    map.insert(k, v);
+                }
+            }
+        } else {
+            self.data = v;
+        }
         Ok(())
     }
 
@@ -53,5 +66,28 @@ impl ModResponse {
     /// Add warning
     pub fn add_warning(&mut self, text: &str) {
         self.warning.push(text.to_string());
+    }
+
+    /// ### Confguration Management
+    ///
+    /// Set changed: <bool> to the data section.
+    /// Used for configuration management
+    pub fn cm_set_changed(&mut self, changed: bool) -> Result<(), serde_json::Error> {
+        let mut out = HashMap::<String, bool>::new();
+        out.insert("changed".to_string(), changed);
+        self.set_data(json!(out))?;
+
+        Ok(())
+    }
+
+    /// ### Configuration management
+    ///
+    /// Set STDOUT of the process
+    pub fn cm_set_stdout(&mut self, data: String) -> Result<(), serde_json::Error> {
+        let mut out = HashMap::<String, String>::new();
+        out.insert("stdout".to_string(), data);
+        self.set_data(json!(out))?;
+
+        Ok(())
     }
 }
