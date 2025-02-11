9ee88c70ee0eb56970b89e8409ff875dbab4f9a6
Bo Maryniuk <bo@maryniuk.net>
2025-02-11 21:42:22 +0100

    Lintfix: remove unnecessary return

--- libsysinspect/src/intp/actproc/modfinder.rs
+++ libsysinspect/src/intp/actproc/modfinder.rs
@@ -291,7 +291,7 @@ impl ModCall {
                             ))),
                             Err(e) => {
                                 log::debug!("STDOUT: {}", out);
-                                return Err(SysinspectError::ModuleError(format!("JSON error: {e}")));
+                                Err(SysinspectError::ModuleError(format!("JSON error: {e}")))
                             }
                         },
                         Err(err) => Err(SysinspectError::ModuleError(format!("Error obtaining the output: {err}"))),
