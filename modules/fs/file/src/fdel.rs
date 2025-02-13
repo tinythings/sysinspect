b027e71fd95355c900ee4ce9d78c4a99cb4163ad
Bo Maryniuk <bo@maryniuk.net>
2025-02-13 13:51:39 +0100

    Implement file delete

--- modules/fs/file/src/fdel.rs
+++ modules/fs/file/src/fdel.rs
@@ -1,4 +1,24 @@
-use libsysinspect::modlib::{response::ModResponse, runtime::ModRequest};
+use libsysinspect::modlib::{
+    response::ModResponse,
+    runtime::{ArgValue, ModRequest},
+};
+use std::path::PathBuf;
 
 /// Do file delete
-pub fn do_delete(rq: &ModRequest, rsp: &mut ModResponse, strict: bool) {}
+pub fn do_delete(rq: &ModRequest, rsp: &mut ModResponse, strict: bool) {
+    let pn = PathBuf::from(rq.args().get("name").unwrap_or(&ArgValue::default()).as_string().unwrap_or_default());
+    if pn.exists() {
+        if let Err(err) = std::fs::remove_file(&pn) {
+            if strict {
+                rsp.set_retcode(1);
+            }
+            rsp.set_message(&format!("Error deleting file \"{}\": {}", pn.to_str().unwrap_or_default(), err));
+            _ = rsp.cm_set_changed(false);
+
+            return;
+        }
+    }
+
+    rsp.set_message(&format!("File \"{}\" was deleted", pn.to_str().unwrap_or_default()));
+    _ = rsp.cm_set_changed(true);
+}
