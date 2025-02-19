6cc07b0f76abebf60d7a97f992cd6b7fc56bb29b
Bo Maryniuk <bogdan.maryniuk@esolution.de>
2025-02-19 17:55:10 +0100

    Define asyncronous action response callback

--- sysminion/src/arcb.rs
+++ sysminion/src/arcb.rs
@@ -1,25 +1,22 @@
+use crate::minion::SysMinion;
+use async_trait::async_trait;
+use libsysinspect::{intp::actproc::response::ActionResponse, reactor::callback::AsyncEventProcessorCallback, SysinspectError};
 use std::sync::Arc;
 
-use libsysinspect::{intp::actproc::response::ActionResponse, reactor::callback::EventProcessorCallback, SysinspectError};
-use tokio::runtime::Handle;
-
-use crate::minion::SysMinion;
-
 #[derive(Debug)]
-pub struct ActionResponseCallback {
+pub struct AsyncActionResponseCallback {
     minion: Arc<SysMinion>,
 }
 
-impl ActionResponseCallback {
+impl AsyncActionResponseCallback {
     pub(crate) fn new(minion: Arc<SysMinion>) -> Self {
         Self { minion }
     }
 }
 
-impl EventProcessorCallback for ActionResponseCallback {
-    fn on_action_response(&mut self, ar: ActionResponse) -> Result<(), SysinspectError> {
-        self.minion.clone().send_callback(ar);
-
-        Ok(())
+#[async_trait]
+impl AsyncEventProcessorCallback for AsyncActionResponseCallback {
+    async fn on_action_response(&mut self, ar: ActionResponse) -> Result<(), SysinspectError> {
+        self.minion.clone().send_callback(ar).await
     }
 }
