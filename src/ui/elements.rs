69b3683460bbf66f10965a577a5d9e9e2b5cbebd
Bo Maryniuk <bogdan.maryniuk@esolutions.de>
2025-03-04 14:59:27 +0100

    Define list elements for the data pointers

--- src/ui/elements.rs
+++ src/ui/elements.rs
@@ -13,14 +13,81 @@ pub enum AlertResult {
     Quit,
 }
 
-/// Cycle is the pointer in the list to the record in the database
-#[derive(Debug, Clone)]
-pub struct Cycle {
-    pub id: u32,
+pub trait DbListItem {
+    fn title(&self) -> String;
+    fn id(&self) -> u32;
 }
 
-impl Cycle {
-    pub fn get_title(&self) -> String {
-        format!("cycle {}", self.id)
+/// Cycle
+/// -----
+///
+/// Cycle is the pointer in the list to the record in the database
+#[derive(Debug, Clone)]
+pub struct CycleListItem {
+    id: u32,
+    title: String,
+}
+
+impl CycleListItem {
+    pub fn new(title: &str, id: u32) -> Self {
+        CycleListItem { id, title: title.to_string() }
+    }
+}
+
+impl DbListItem for CycleListItem {
+    fn title(&self) -> String {
+        format!("{} {}", self.title, self.id())
+    }
+
+    fn id(&self) -> u32 {
+        self.id
+    }
+}
+
+/// Event
+/// -----
+#[derive(Debug, Clone)]
+pub struct EventListItem {
+    id: u32,
+    title: String,
+}
+
+impl EventListItem {
+    pub fn new(title: &str, id: u32) -> Self {
+        EventListItem { id, title: title.to_string() }
+    }
+}
+
+impl DbListItem for EventListItem {
+    fn title(&self) -> String {
+        format!("{}: {}", self.title, self.id())
+    }
+
+    fn id(&self) -> u32 {
+        self.id
+    }
+}
+
+/// Minion
+/// ------
+#[derive(Debug, Clone)]
+pub struct MinionListItem {
+    id: u32,
+    title: String,
+}
+
+impl MinionListItem {
+    pub fn new(title: &str, id: u32) -> Self {
+        MinionListItem { id, title: title.to_string() }
+    }
+}
+
+impl DbListItem for MinionListItem {
+    fn title(&self) -> String {
+        format!("{}: {}", self.title, self.id())
+    }
+
+    fn id(&self) -> u32 {
+        self.id
     }
 }
