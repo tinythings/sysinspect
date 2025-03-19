62c936c0d1f66dca15f0a2f694fac15e48336c1b
Bo Maryniuk <bogdan.maryniuk@esolutions.de>
2025-03-19 18:05:57 +0100

    Add an utility function to get SSH_CLIENT. Needs to be moved!

--- libsetup/src/lib.rs
+++ libsetup/src/lib.rs
@@ -1,1 +1,19 @@
+use std::env;
+
 pub mod mnsetup;
+
+/// Get SSH user IP.
+///
+/// This basically reaps-off $SSH_CLIENT variable from the environment,
+/// in a hope to find out the IP address of the remote host,
+/// which is assumed to be a master host.
+pub fn get_ssh_client_ip() -> Option<String> {
+    if let Ok(ssh_client) = env::var("SSH_CLIENT") {
+        let parts: Vec<&str> = ssh_client.split_whitespace().collect();
+        if parts.len() > 0 {
+            return Some(parts[0].to_string());
+        }
+    }
+
+    None
+}
