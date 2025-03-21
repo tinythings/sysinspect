ada79b5bf55b75c6c021f93d780618eea9a6b4c3
Bo Maryniuk <bo@maryniuk.net>
2025-03-21 18:38:30 +0100

    Implement module addition and indexing

--- libmodpak/src/lib.rs
+++ libmodpak/src/lib.rs
@@ -1,5 +1,7 @@
+use colored::Colorize;
+use goblin::{Object, elf::header};
 use libsysinspect::SysinspectError;
-use mpk::{ModPackModule, ModPakArch};
+use mpk::{ModPackModule, ModPakMetadata, ModPakRepoIndex};
 use std::{fs, path::PathBuf};
 
 pub mod mpk;
@@ -9,9 +11,11 @@ ModPack is a library for creating and managing modules, providing a way to defin
 their dependencies, and their architecture.
 */
 
+static REPO_INDEX: &str = "repo.index";
 /// ModPakRepo is a repository for storing and managing modules.
 pub struct SysInspectModPak {
     root: PathBuf,
+    idx: ModPakRepoIndex,
 }
 
 impl SysInspectModPak {
@@ -20,15 +24,97 @@ impl SysInspectModPak {
         if !root.exists() {
             log::info!("Creating module repository at {}", root.display());
             std::fs::create_dir_all(&root)?;
+            fs::write(&root.join(REPO_INDEX), ModPakRepoIndex::new().to_yaml()?)?;
         }
 
-        Ok(Self { root })
+        Ok(Self { root: root.clone(), idx: ModPakRepoIndex::from_yaml(&fs::read_to_string(root.join(REPO_INDEX))?)? })
+    }
+
+    /// Get osabi label
+    fn get_osabi_label(osabi: u8) -> &'static str {
+        match osabi {
+            header::ELFOSABI_SYSV => "sysv",
+            header::ELFOSABI_NETBSD => "netbsd",
+            header::ELFOSABI_LINUX => "linux",
+            header::ELFOSABI_FREEBSD => "freebsd",
+            header::ELFOSABI_OPENBSD => "openbsd",
+            header::ELFOSABI_ARM => "arm",
+            header::ELFOSABI_ARM_AEABI => "arm-eabi",
+            header::ELFOSABI_STANDALONE => "standalone",
+            _ => "any",
+        }
+    }
+
+    /// Extract module subpath from its name
+    fn after<'a>(full_path: &'a str, sub: &'a str) -> &'a str {
+        if let Some(index) = full_path.find(sub) {
+            let result = &full_path[index..];
+            if result.len() == sub.len() { sub } else { result }
+        } else {
+            sub
+        }
     }
 
     /// Add an existing binary module.
-    pub fn add_module(&self, p: ModPackModule) -> Result<(), SysinspectError> {
-        let path = self.root.join(p.get_subpath());
-        log::info!("Adding module {}", path.display());
+    pub fn add_module(&mut self, meta: ModPakMetadata) -> Result<(), SysinspectError> {
+        let path = fs::canonicalize(meta.get_path())?;
+        log::info!("Adding module {}", path.display().to_string().bright_yellow());
+
+        let buff = fs::read(path)?;
+        let (is_bin, arch, p) = match Object::parse(&buff).unwrap() {
+            Object::Elf(elf) => {
+                let x = match elf.header.e_machine {
+                    goblin::elf::header::EM_X86_64 => {
+                        log::info!("Architectire: x86_64 ELF");
+                        "x86_64"
+                    }
+                    goblin::elf::header::EM_ARM => {
+                        log::info!("Architecture: ARM ELF");
+                        "ARM"
+                    }
+                    goblin::elf::header::EM_AARCH64 => {
+                        log::info!("Architecture: ARM64 ELF");
+                        "ARM64"
+                    }
+                    _ => {
+                        return Err(SysinspectError::MasterGeneralError(
+                            "Module is not a supported ELF architecture".to_string(),
+                        ));
+                    }
+                };
+                (true, x, Self::get_osabi_label(elf.header.e_ident[header::EI_OSABI as usize]))
+            }
+            _ => {
+                log::info!("Module is not an executable ELF file");
+                (false, "noarch", "any")
+            }
+        };
+
+        log::info!("Platform: {}", p);
+        let x = PathBuf::from(Self::after(
+            meta.get_path().to_str().unwrap_or_default(),
+            meta.get_subpath().to_str().unwrap_or_default(),
+        ));
+        let subpath = PathBuf::from(format!("{}/{}/{}", if is_bin { "bin" } else { "script" }, p, arch)).join(x);
+        log::debug!("Subpath: {}", subpath.display().to_string().bright_yellow());
+        log::info!("Adding module");
+        self.root.join(&subpath).parent().map(|p| {
+            if !p.exists() {
+                log::debug!("Creating directory {}", p.display().to_string().bright_yellow());
+                std::fs::create_dir_all(p).unwrap();
+            }
+        });
+        log::debug!("Copying module to {}", self.root.join(&subpath).display().to_string().bright_yellow());
+        std::fs::copy(meta.get_path(), self.root.join(&subpath))?;
+
+        self.idx
+            .add_module(meta.get_name().as_str(), p, arch)
+            .map_err(|e| SysinspectError::MasterGeneralError(format!("Failed to add module to index: {}", e)))?;
+        log::debug!("Writing index to {}", self.root.join(REPO_INDEX).display().to_string().bright_yellow());
+        fs::write(self.root.join(REPO_INDEX), self.idx.to_yaml()?)?;
+        log::debug!("Module {} added to index", meta.get_name().bright_yellow());
+        log::info!("Module {} added successfully", meta.get_name().bright_yellow());
+
         Ok(())
     }
 
@@ -40,7 +126,7 @@ impl SysInspectModPak {
                 let path = entry.path();
                 if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
                     if let Some(arch) = path.parent().and_then(|p| p.file_name()).and_then(|n| n.to_str()) {
-                        modules.push(ModPackModule::new(name.to_string(), ModPakArch::Noarch, false)?);
+                        modules.push(ModPackModule::new(name.to_string(), false)?);
                     }
                 }
             }
@@ -48,17 +134,13 @@ impl SysInspectModPak {
         Ok(modules)
     }
 
-    pub fn remove_module(&self, name: &str, arch: Option<ModPakArch>) -> Result<(), SysinspectError> {
-        let path = fs::canonicalize(self.get_module(name, arch)?)?;
-        if path.exists() {
-            std::fs::remove_dir_all(path)?;
-        }
+    pub fn remove_module(&self, name: &str) -> Result<(), SysinspectError> {
         Ok(())
     }
 
     /// Get module location.
     /// If the module is a binary module, it will return the path to the binary.
-    pub fn get_module(&self, name: &str, arch: Option<ModPakArch>) -> Result<String, SysinspectError> {
+    pub fn get_module(&self, name: &str) -> Result<String, SysinspectError> {
         Ok("".to_string())
     }
 }
--- libmodpak/src/mpk.rs
+++ libmodpak/src/mpk.rs
@@ -1,55 +1,113 @@
-use std::path::{Path, PathBuf};
-
+use indexmap::IndexMap;
 use libsysinspect::SysinspectError;
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum ModPakArch {
-    X86,
-    X64,
-    ARM,
-    ARM64,
-    Noarch,
+pub struct ModPakRepoIndex {
+    /// Platform -> Architecture -> Module name
+    /// e.g. "linux" -> "x86_64" -> "fs.file"
+    platform: IndexMap<String, IndexMap<String, IndexMap<String, String>>>,
+}
+
+impl ModPakRepoIndex {
+    /// Creates a new ModPakRepoIndex.
+    pub fn new() -> Self {
+        Self { platform: IndexMap::new() }
+    }
+
+    /// Adds a module to the index.
+    pub fn add_module(&mut self, name: &str, platform: &str, arch: &str) -> Result<(), SysinspectError> {
+        self.platform
+            .entry(platform.to_string())
+            .or_insert_with(IndexMap::new)
+            .entry(arch.to_string())
+            .or_insert_with(IndexMap::new)
+            .insert(name.to_string(), name.to_string());
+
+        Ok(())
+    }
+
+    /// Deletes a module from the index.
+    pub fn del_module(&mut self, name: &str, platform: &str, arch: &str) -> Result<(), SysinspectError> {
+        if let Some(platform_map) = self.platform.get_mut(platform) {
+            if let Some(arch_map) = platform_map.get_mut(arch) {
+                arch_map.shift_remove(name);
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Serializes the index to a YAML string.
+    pub fn to_yaml(&self) -> Result<String, SysinspectError> {
+        let yaml = serde_yaml::to_string(self)?;
+        Ok(yaml)
+    }
+
+    /// Deserializes a YAML string to a ModPakRepoIndex.
+    pub fn from_yaml(yaml: &str) -> Result<Self, SysinspectError> {
+        let index: ModPakRepoIndex = serde_yaml::from_str(yaml)?;
+        Ok(index)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct ModPakMetadata {
+    path: PathBuf,
+    name: String,
+}
+
+impl ModPakMetadata {
+    /// Returns the path to the module.
+    pub fn get_path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    /// Returns the name of the module.
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn get_subpath(&self) -> PathBuf {
+        self.get_name().trim_start_matches('.').trim_end_matches('.').to_string().replace('.', "/").into()
+    }
+
+    pub fn from_cli_matches(matches: &clap::ArgMatches) -> Self {
+        let mut mpm = ModPakMetadata::default();
+
+        if let Some(path) = matches.get_one::<String>("path") {
+            mpm.path = PathBuf::from(path);
+        }
+
+        if let Some(name) = matches.get_one::<String>("name") {
+            mpm.name = name.clone();
+        }
+
+        mpm
+    }
 }
 
 /// Module is a single unit of functionality that can be used in a ModPack.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ModPackModule {
-    arch: ModPakArch,
     name: String, // Module name as in model, e.g. "fs.file"
     binary: bool,
 }
 
 impl ModPackModule {
     /// Creates a new ModPackModule with the given name and architecture.
-    pub fn new(name: String, arch: ModPakArch, binary: bool) -> Result<Self, SysinspectError> {
+    pub fn new(name: String, binary: bool) -> Result<Self, SysinspectError> {
         if !name.contains(".") {
             return Err(SysinspectError::InvalidModuleName(format!("Module \"{}\" must have a namespace", name)));
         }
 
-        Ok(Self { name: name.trim_start_matches('.').trim_end_matches('.').to_string(), arch, binary })
+        Ok(Self { name: name.trim_start_matches('.').trim_end_matches('.').to_string(), binary })
     }
 
     fn get_name_subpath(&self) -> String {
         self.name.clone().replace('.', "/")
     }
-
-    /// Returns architecture of the module.
-    fn get_arch_label(&self) -> String {
-        match self.arch {
-            ModPakArch::X86 => "x86".to_string(),
-            ModPakArch::X64 => "x64".to_string(),
-            ModPakArch::ARM => "arm".to_string(),
-            ModPakArch::ARM64 => "arm64".to_string(),
-            ModPakArch::Noarch => "noarch".to_string(),
-        }
-    }
-
-    /// Get subpath to the module
-    pub fn get_subpath(&self) -> PathBuf {
-        Path::new("modules").join(self.get_arch_label()).join(&self.name)
-    }
-
     /// Returns true if the module is a binary module.
     pub fn is_binary(&self) -> bool {
         self.binary
