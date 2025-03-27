1fd75d50899b578a3be451c4054a5273dd615919
Bo Maryniuk <bo@maryniuk.net>
2025-03-27 17:25:36 +0100

    Update CLI command descriptions, add sync option

--- src/clidef.rs
+++ src/clidef.rs
@@ -18,32 +18,32 @@ pub fn cli(version: &'static str) -> Command {
         .override_usage(format!("{} [OPTIONS] [FILTERS]", APPNAME))
 
         // Module management
-        .subcommand(Command::new("module").about("Add, remove or list modules").styles(styles.clone()).disable_help_flag(true)
+        .subcommand(Command::new("module").about("Add, remove, or list modules").styles(styles.clone()).disable_help_flag(true)
             .arg(Arg::new("add").short('A').long("add").action(ArgAction::SetTrue).help("Add a module to the repository").conflicts_with_all(["remove", "list"]))
             .arg(Arg::new("remove").short('R').long("remove").action(ArgAction::SetTrue).help("Remove a module from the repository").conflicts_with_all(["add", "list"]))
             .arg(Arg::new("list").short('L').long("list").action(ArgAction::SetTrue).help("List all modules in the repository").conflicts_with_all(["add", "remove"]))
 
-            .arg(Arg::new("lib").short('l').long("lib").action(ArgAction::SetTrue).help("Module is a library (usually Python scripts)").conflicts_with_all(["list"]))
+            .arg(Arg::new("lib").short('l').long("lib").action(ArgAction::SetTrue).help("Specify that the module is a library (usually Python scripts)").conflicts_with_all(["list"]))
 
-            .arg(Arg::new("name").short('n').long("name").required_unless_present_any(["help", "list", "lib"]).help("Module name"))
-            .arg(Arg::new("path").short('p').long("path").required_unless_present_any(["help", "list", "remove"]).help("Path to the module (or library)"))
-            .arg(Arg::new("descr").short('d').long("descr").required_unless_present_any(["help", "list", "lib", "remove"]).help("Description of the module"))
-            .arg(Arg::new("arch").short('a').long("arch").help("Module architecture (x86, x64, arm, arm64, noarch)").default_value("noarch"))
+            .arg(Arg::new("name").short('n').long("name").required_unless_present_any(["help", "list", "lib"]).help("Specify the module name"))
+            .arg(Arg::new("path").short('p').long("path").required_unless_present_any(["help", "list", "remove"]).help("Specify the path to the module (or library)"))
+            .arg(Arg::new("descr").short('d').long("descr").required_unless_present_any(["help", "list", "lib", "remove"]).help("Provide a description of the module"))
+            .arg(Arg::new("arch").short('a').long("arch").help("Specify the module architecture (x86, x64, arm, arm64, noarch)").default_value("noarch"))
 
-            .arg(Arg::new("help").short('h').long("help").action(ArgAction::SetTrue).help("Display help on this command"))
+            .arg(Arg::new("help").short('h').long("help").action(ArgAction::SetTrue).help("Display help for this command"))
         )
 
         // Sysinspect
         .next_help_heading("Main")
         .arg(
             Arg::new("path")
-                .help("Specify model path that needs to be requested")
+                .help("Specify the model path that needs to be requested")
                 .required(false)
                 .index(1)
         )
         .arg(
             Arg::new("query")
-                .help("Minions to query")
+                .help("Specify the minions to query")
                 .required(false)
                 .index(2)
         )
@@ -60,29 +60,29 @@ pub fn cli(version: &'static str) -> Command {
         // Config
         .arg(
             Arg::new("model")
-                .short('m')
-                .long("model")
-                .help("System description model")
+            .short('m')
+            .long("model")
+            .help("Specify the system description model")
         )
         .arg(
             Arg::new("labels")
-                .short('l')
-                .long("labels")
-                .help("Select only specific labels from the checkbook (comma-separated)")
-                .conflicts_with_all(["entities"])
+            .short('l')
+            .long("labels")
+            .help("Select only specific labels from the checkbook (comma-separated values)")
+            .conflicts_with_all(["entities"])
         )
         .arg(
             Arg::new("entities")
-                .short('e')
-                .long("entities")
-                .help("Select only specific entities from the inventory (comma-separated)")
-                .conflicts_with_all(["labels"])
+            .short('e')
+            .long("entities")
+            .help("Select only specific entities from the inventory (comma-separated values)")
+            .conflicts_with_all(["labels"])
         )
         .arg(
             Arg::new("state")
                 .short('s')
                 .long("state")
-                .help("Specify a state to be processed. If none specified, default is taken ($)")
+                .help("Specify a state to process. If none is specified, the default state ($) is used.")
         )
 
         // Cluster
@@ -92,18 +92,24 @@ pub fn cli(version: &'static str) -> Command {
             .short('u')
             .long("ui")
             .action(ArgAction::SetTrue)
-            .help("Run terminal user interface app (TUI) for the review of the results")
+            .help("Run the terminal user interface (TUI) application to review the results")
         )
         .arg(
             Arg::new("unregister")
                 .long("unregister")
-                .help("Unregister a minion by its System Id. New registration will be required.")
+                .help("Unregister a minion by its System ID. A new registration will be required.")
+        )
+        .arg(
+            Arg::new("sync")
+                .long("sync")
+                .action(ArgAction::SetTrue)
+                .help(format!("Sync the {} for all artefacts (modules, libraries, traits etc)", "entire cluster".bright_red()))
         )
         .arg(
             Arg::new("shutdown")
                 .long("shutdown")
                 .action(ArgAction::SetTrue)
-                .help(format!("Notify the running master to shutdown the {}, be careful! :)", "entire cluster".bright_red()))
+                .help(format!("Notify the running master to shut down the {}, be careful! :)", "entire cluster".bright_red()))
         )
 
         .next_help_heading("Model")
@@ -111,7 +117,7 @@ pub fn cli(version: &'static str) -> Command {
             Arg::new("list-handlers")
                 .long("list-handlers")
                 .action(ArgAction::SetTrue)
-                .help("List available event handler Ids")
+                .help("List available event handler IDs")
         )
 
 
@@ -121,14 +127,14 @@ pub fn cli(version: &'static str) -> Command {
             Arg::new("config")
                 .short('c')
                 .long("config")
-                .help("Specify alternative configuration")
+                .help("Specify an alternative configuration")
         )
         .arg(
             Arg::new("debug")
                 .short('d')
                 .long("debug")
                 .action(ArgAction::Count)
-                .help("Set debug mode for more verbose output. Increase this flag for more verbosity."),
+                .help("Enable debug mode for more verbose output. Increase this flag for greater verbosity."),
         )
         .arg(
             Arg::new("help")
@@ -142,14 +148,14 @@ pub fn cli(version: &'static str) -> Command {
                 .short('v')
                 .long("version")
                 .action(ArgAction::SetTrue)
-                .help("Get current version."),
+                .help("Get the current version."),
         )
-        .disable_help_flag(true) // Otherwise it is displayed in a wrong position
+        .disable_help_flag(true) // Otherwise, it is displayed in the wrong position
         .disable_version_flag(true)
         .disable_colored_help(false)
         .styles(styles)
-        .after_help("NOTE: This tool is in very early development.
-      If it doesn't work for you, please fill a bug report here:
+        .after_help("NOTE: SysInspect is in early development.
+      If it does not work as expected, please submit a bug report here:
       https://github.com/tinythings/sysinspect/issues\n".bright_yellow().to_string())
 }
 
