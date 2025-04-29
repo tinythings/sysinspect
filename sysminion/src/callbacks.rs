d3ae2fe478d21ea6fe3f499fb62edd4aa3286d12
Bo Maryniuk <bogdan.maryniuk@esolutions.de>
2025-04-29 12:30:25 +0200

    Create model response callback (called after model call at the end of all events)

--- sysminion/src/callbacks.rs
+++ sysminion/src/callbacks.rs
@@ -1,8 +1,9 @@
-use crate::minion::SysMinion;
+use crate::minion::{self, SysMinion};
 use async_trait::async_trait;
 use libsysinspect::{SysinspectError, intp::actproc::response::ActionResponse, reactor::callback::EventProcessorCallback};
 use std::sync::Arc;
 
+// Callback for action response fired after each action/event.
 #[derive(Debug)]
 pub struct ActionResponseCallback {
     cid: String,
@@ -22,5 +23,26 @@ impl EventProcessorCallback for ActionResponseCallback {
     async fn on_action_response(&mut self, mut ar: ActionResponse) -> Result<(), SysinspectError> {
         ar.set_cid(self.cid.to_owned());
         self.minion.clone().send_callback(ar).await
+    }
+}
+
+/// Callback for model response at the end of the model cycle.
+#[derive(Debug)]
+pub struct ModelResponseCallback {
+    minion: Arc<SysMinion>,
+    cid: String,
+}
+
+impl ModelResponseCallback {
+    pub(crate) fn new(minion: Arc<SysMinion>, cid: &str) -> Self {
+        Self { minion, cid: cid.to_owned() }
+    }
+}
+
+#[async_trait]
+impl EventProcessorCallback for ModelResponseCallback {
+    async fn on_action_response(&mut self, mut ar: ActionResponse) -> Result<(), SysinspectError> {
+        ar.set_cid(self.cid.to_owned());
+        self.minion.clone().send_fin_callback(ar).await
     }
 }
