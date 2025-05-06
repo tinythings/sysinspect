4b53d910f20809045e037da8d12d05b05f12ee1c
Bo Maryniuk <bo@maryniuk.net>
2025-05-06 17:50:21 +0200

    Set telemetry confing from the model to the action response, so the master knows precisely what configuration to use

--- sysminion/src/callbacks.rs
+++ sysminion/src/callbacks.rs
@@ -1,6 +1,9 @@
 use crate::minion::SysMinion;
 use async_trait::async_trait;
-use libsysinspect::{SysinspectError, intp::actproc::response::ActionResponse, reactor::callback::EventProcessorCallback};
+use libsysinspect::{
+    SysinspectError, intp::actproc::response::ActionResponse, mdescr::telemetry::TelemetrySpec,
+    reactor::callback::EventProcessorCallback,
+};
 use std::sync::Arc;
 
 // Callback for action response fired after each action/event.
@@ -8,13 +11,14 @@ use std::sync::Arc;
 pub struct ActionResponseCallback {
     cid: String,
     minion: Arc<SysMinion>,
+    telemetry_config: Option<TelemetrySpec>,
 }
 
 impl ActionResponseCallback {
     /// The `cid` (Cycle ID) is used to identify the master cycle, so the response
     /// is registered with the other minions, grouped into the same call session.
     pub(crate) fn new(minion: Arc<SysMinion>, cid: &str) -> Self {
-        Self { minion, cid: cid.to_owned() }
+        Self { minion, cid: cid.to_owned(), telemetry_config: None }
     }
 }
 
@@ -22,7 +26,14 @@ impl ActionResponseCallback {
 impl EventProcessorCallback for ActionResponseCallback {
     async fn on_action_response(&mut self, mut ar: ActionResponse) -> Result<(), SysinspectError> {
         ar.set_cid(self.cid.to_owned());
+        if let Some(tcfg) = &self.telemetry_config {
+            ar.set_telemetry_config(tcfg.minion());
+        }
         self.minion.clone().send_callback(ar).await
+    }
+
+    fn set_telemetry_config(&mut self, _telemetry_config: Option<TelemetrySpec>) {
+        self.telemetry_config = _telemetry_config;
     }
 }
 
@@ -31,11 +42,12 @@ impl EventProcessorCallback for ActionResponseCallback {
 pub struct ModelResponseCallback {
     minion: Arc<SysMinion>,
     cid: String,
+    telemetry_config: Option<TelemetrySpec>,
 }
 
 impl ModelResponseCallback {
     pub(crate) fn new(minion: Arc<SysMinion>, cid: &str) -> Self {
-        Self { minion, cid: cid.to_owned() }
+        Self { minion, cid: cid.to_owned(), telemetry_config: None }
     }
 }
 
@@ -43,6 +55,13 @@ impl ModelResponseCallback {
 impl EventProcessorCallback for ModelResponseCallback {
     async fn on_action_response(&mut self, mut ar: ActionResponse) -> Result<(), SysinspectError> {
         ar.set_cid(self.cid.to_owned());
+        if let Some(tcfg) = &self.telemetry_config {
+            ar.set_telemetry_config(tcfg.model());
+        }
         self.minion.clone().send_fin_callback(ar).await
+    }
+
+    fn set_telemetry_config(&mut self, _telemetry_config: Option<TelemetrySpec>) {
+        self.telemetry_config = _telemetry_config;
     }
 }
