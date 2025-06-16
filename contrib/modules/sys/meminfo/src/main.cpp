8e9305840117e039e21fe6321e3de718846a3db0
Bo Maryniuk <bogdan.maryniuk@esolutions.de>
2025-06-16 17:03:51 +0200

    Add conversion units

--- contrib/modules/sys/meminfo/src/main.cpp
+++ contrib/modules/sys/meminfo/src/main.cpp
@@ -24,6 +24,31 @@ int main() {
         return 1;
     }
 
+    // What unit?
+    std::string unit = "kb";
+    if (jin.contains("args")) {
+        if (jin["args"].contains("unit")) {
+            unit = jin["args"]["unit"].get<std::string>();
+            if (!(unit == "bt" || unit == "kb" || unit == "mb" || unit == "gb")) {
+                unit = "kb";
+            }
+            std::transform(unit.begin(), unit.end(), unit.begin(), ::tolower);
+        }
+    }
+
+    auto u = [&](long v) -> double {
+        if (unit == "bt") {
+            return static_cast<double>(v) * 1024.0;
+        } else if (unit == "kb") {
+            return static_cast<double>(v);
+        } else if (unit == "mb") {
+            return static_cast<double>(v) / 1024.0;
+        } else if (unit == "gb") {
+            return static_cast<double>(v) / (1024.0 * 1024.0);
+        }
+        return static_cast<double>(v);
+    };
+
     MemInfo memInfo;
 
     // JSON output
@@ -31,11 +56,15 @@ int main() {
     jout["retcode"] = 0;
     jout["message"] = "Data has been collected successfully";
 
+    jout["data"] = json::object();
+    jout["data"]["changed"] = true;
+    jout["data"]["unit"] = unit;
+
     for (const auto &opt : jin.contains("opts") ? jin["opts"] : jin["options"]) {
         static const std::map<std::string, std::function<void()>> f = {
-            {"free", [&]() { jout["data"]["mem-free"] = memInfo.getMemFree(); }},
-            {"total", [&]() { jout["data"]["mem-total"] = memInfo.getMemTotal(); }},
-            {"avail", [&]() { jout["data"]["mem-available"] = memInfo.getMemAvailable(); }}};
+            {"free", [&]() { jout["data"]["mem-free"] = u(memInfo.getMemFree()); }},
+            {"total", [&]() { jout["data"]["mem-total"] = u(memInfo.getMemTotal()); }},
+            {"avail", [&]() { jout["data"]["mem-available"] = u(memInfo.getMemAvailable()); }}};
 
         auto it = f.find(opt.get<std::string>());
         if (it != f.end()) {
