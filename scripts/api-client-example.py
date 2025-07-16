84872a9e81b8b8d81321b0441e01d95d0aa4ef2e
Bo Maryniuk <bo@maryniuk.net>
2025-07-16 17:44:27 +0200

    Add example post

--- scripts/api-client-example.py
+++ scripts/api-client-example.py
@@ -7,3 +7,13 @@ req.add_header("Content-Type", "application/json")
 with urllib.request.urlopen(req) as resp:
     out = json.loads(resp.read().decode())
     print(out)
+
+
+query = json.dumps({"query": "cm/file-ops;*;;;metaid:ca375102-6184-11f0-893f-c32f44e18d2c,tgt:something"})
+req = urllib.request.Request("http://127.0.0.1:4202/api/v1/query", data=query.encode("utf-8"), method="POST")
+req.add_header("Content-Type", "application/json")
+
+with urllib.request.urlopen(req) as resp:
+    out = json.loads(resp.read().decode())
+    print(out)
+
