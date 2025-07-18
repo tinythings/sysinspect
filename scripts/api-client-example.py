bd7b06f87e3791064f736f6dea1a939cd96deef9
Bo Maryniuk <bo@maryniuk.net>
2025-07-18 16:04:33 +0200

    Update API python example

--- scripts/api-client-example.py
+++ scripts/api-client-example.py
@@ -1,19 +1,29 @@
 import urllib.request
 import json
 
-req = urllib.request.Request("http://127.0.0.1:4202/api/v1/health", data=b"", method="POST")
-req.add_header("Content-Type", "application/json")
+def call_api(endpoint, data=None):
+    """
+    Call the Sysinspect API at the specified endpoint with the given data.
+    """
 
-with urllib.request.urlopen(req) as resp:
-    out = json.loads(resp.read().decode())
-    print(out)
+    url = f"http://127.0.0.1:4202/api/v1/{endpoint}"
+    req = urllib.request.Request(url, data=data, method="POST")
+    req.add_header("Content-Type", "application/json")
+
+    with urllib.request.urlopen(req) as resp:
+        return json.loads(resp.read().decode())
 
 
-query = json.dumps({"query": "cm/file-ops;*;;;metaid:ca375102-6184-11f0-893f-c32f44e18d2c,tgt:something"})
-req = urllib.request.Request("http://127.0.0.1:4202/api/v1/query", data=query.encode("utf-8"), method="POST")
-req.add_header("Content-Type", "application/json")
+query = json.dumps({
+    "model": "cm/file-ops",
+    "query": "*",
+    "traits": "",
+    "mid": "",
+    "context": {
+        "metaid": "ca375102-6184-11f0-893f-c32f44e18d2c",
+        "tgt": "something"
+    }
+}).encode("utf-8")
 
-with urllib.request.urlopen(req) as resp:
-    out = json.loads(resp.read().decode())
-    print(out)
-
+out = call_api("query", data=query)
+print(out)
