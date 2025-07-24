6326b4ca58509b47bdea17cb06c124025e5ee963
Bo Maryniuk <bo@maryniuk.net>
2025-07-24 17:24:06 +0200

    Decode SID RSA cipher

--- libwebapi/src/api/v1/pkeys.rs
+++ libwebapi/src/api/v1/pkeys.rs
@@ -1,12 +1,13 @@
 use crate::{MasterInterfaceType, api::v1::TAG_RSAKEYS, keystore::get_webapi_keystore, sessions::get_session_store};
 use actix_web::{HttpResponse, Responder, post, web};
+use base64::Engine;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 /// Push to push a user public key to store on the server.
 #[derive(Deserialize, ToSchema)]
 pub struct PubKeyRequest {
-    pub sid: String,
+    pub sid_cipher: String,
     pub key: String,
 }
 
@@ -36,19 +37,29 @@ pub struct PubKeyError {
 pub async fn pushkey_handler(master: web::Data<MasterInterfaceType>, body: web::Json<PubKeyRequest>) -> impl Responder {
     let master = master.lock().await;
     let cfg = master.cfg().await;
-
-    // Check if API is in dev mode or SID is valid
-    let uid = get_session_store().lock().unwrap().uid(&body.sid);
-    if !cfg.api_devmode() && uid.is_none() {
-        return HttpResponse::BadRequest().json(PubKeyError { error: "Invalid session ID".to_string() });
-    }
-
     let keystore = match get_webapi_keystore(cfg) {
         Ok(path) => path,
         Err(err) => {
             return HttpResponse::BadRequest().json(PubKeyError { error: format!("Internal error. Failed to init keystore: {err}") });
         }
     };
+    let sid = match base64::engine::general_purpose::STANDARD.decode(&body.sid_cipher) {
+        Ok(bytes) => match String::from_utf8(bytes) {
+            Ok(s) => s,
+            Err(_) => {
+                return HttpResponse::BadRequest().json(PubKeyError { error: "Invalid base64 encoding in sid_cipher".to_string() });
+            }
+        },
+        Err(_) => {
+            return HttpResponse::BadRequest().json(PubKeyError { error: "Failed to decode base64 sid_cipher".to_string() });
+        }
+    };
+
+    // Check if API is in dev mode or SID is valid
+    let uid = get_session_store().lock().unwrap().uid(&sid);
+    if !cfg.api_devmode() && uid.is_none() {
+        return HttpResponse::BadRequest().json(PubKeyError { error: "Invalid session ID".to_string() });
+    }
 
     if let Err(err) = keystore.save_key(&uid.unwrap_or_else(|| "developer".to_string()), &body.key) {
         return HttpResponse::BadRequest().json(PubKeyError { error: format!("Failed to save public key: {err}") });
