6403eb05791dc06dbbc71fa04ead21875dcc7cac
Bo Maryniuk <bo@maryniuk.net>
2026-02-23 15:52:33 +0100

    Cosmetic: mute warning on unused variable

--- libdatastore/src/main.rs
+++ libdatastore/src/main.rs
@@ -5,6 +5,6 @@ fn main() -> anyhow::Result<()> {
     let cfg = DataStorageConfig::new().expiration("3 days")?.max_item_size("1 gb")?.max_overall_size("20 gb")?;
 
     let storage = DataStorage::new(cfg, "/tmp/store")?;
-    let same = storage.meta(&storage.add(env::current_exe()?.to_str().unwrap())?.sha256)?;
+    let _same = storage.meta(&storage.add(env::current_exe()?.to_str().unwrap())?.sha256)?;
     Ok(())
 }
