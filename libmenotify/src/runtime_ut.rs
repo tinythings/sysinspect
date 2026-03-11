8bae9eb1f5933dcca6491d7fd4494825caf0ea12
Bo Maryniuk <bo@maryniuk.net>
2026-03-11 11:45:44 +0100

    Add runtime UT for script paths

--- libmenotify/src/runtime_ut.rs
+++ libmenotify/src/runtime_ut.rs
@@ -1,23 +1,28 @@
 use crate::MeNotifyRuntime;
 use std::{fs, path::Path};
 
-#[test]
-fn resolves_script_path_from_listener_and_env_root() {
-    let tmp = tempfile::tempdir().expect("tempdir should be created");
+fn with_sharelib(root: &std::path::Path, f: impl FnOnce()) {
     unsafe {
-        std::env::set_var("SYSINSPECT_SHARELIB_ROOT", tmp.path());
+        std::env::set_var("SYSINSPECT_SHARELIB_ROOT", root);
     }
-    let runtime = MeNotifyRuntime::new("demo".to_string(), "menotify.sample".to_string());
+    f();
     unsafe {
         std::env::remove_var("SYSINSPECT_SHARELIB_ROOT");
     }
+}
 
-    assert_eq!(runtime.script_root(), Path::new(tmp.path()).join("lib/sensors/lua54"));
-    assert_eq!(runtime.site_root(), Path::new(tmp.path()).join("lib/sensors/lua54/site-lua"));
-    assert_eq!(
-        runtime.script_path().expect("script path should resolve"),
-        Path::new(tmp.path()).join("lib/sensors/lua54/sample.lua")
-    );
+#[test]
+fn resolves_script_path_from_listener_and_env_root() {
+    let tmp = tempfile::tempdir().expect("tempdir should be created");
+    with_sharelib(tmp.path(), || {
+        let runtime = MeNotifyRuntime::new("demo".to_string(), "menotify.sample".to_string());
+        assert_eq!(runtime.script_root(), Path::new(tmp.path()).join("lib/sensors/lua54"));
+        assert_eq!(runtime.site_root(), Path::new(tmp.path()).join("lib/sensors/lua54/site-lua"));
+        assert_eq!(
+            runtime.script_path().expect("script path should resolve"),
+            Path::new(tmp.path()).join("lib/sensors/lua54/sample.lua")
+        );
+    });
 }
 
 #[test]
@@ -27,15 +32,10 @@ fn require_script_accepts_existing_file() {
     fs::create_dir_all(&root).expect("script root should be created");
     fs::write(root.join("demo.lua"), "return {}\n").expect("script file should be written");
 
-    unsafe {
-        std::env::set_var("SYSINSPECT_SHARELIB_ROOT", tmp.path());
-    }
-    let runtime = MeNotifyRuntime::new("demo".to_string(), "menotify.demo".to_string());
-    unsafe {
-        std::env::remove_var("SYSINSPECT_SHARELIB_ROOT");
-    }
-
-    assert_eq!(runtime.require_script().expect("script should exist"), root.join("demo.lua"));
+    with_sharelib(tmp.path(), || {
+        let runtime = MeNotifyRuntime::new("demo".to_string(), "menotify.demo".to_string());
+        assert_eq!(runtime.require_script().expect("script should exist"), root.join("demo.lua"));
+    });
 }
 
 #[test]
