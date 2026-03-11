220c43e36218b2cdfa2c655ce238239ad04590e7
Bo Maryniuk <bo@maryniuk.net>
2026-03-11 16:09:56 +0100

    OK, clippy, ok...

--- libsensors/src/lib.rs
+++ libsensors/src/lib.rs
@@ -109,7 +109,7 @@ fn collect_chunks(p: &Path) -> Vec<PathBuf> {
     chunks
 }
 
-#[allow(dead_code)]
+#[cfg(test)]
 fn merged_events_yaml(p: &Path) -> Result<serde_yaml::Value, SysinspectError> {
     let mut events_map: serde_yaml::Mapping = serde_yaml::Mapping::new();
     for path in collect_chunks(p) {
