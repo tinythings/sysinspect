9605b28524e9f492a933fa17d390329e33c66601
Bo Maryniuk <bo@maryniuk.net>
2026-03-12 23:42:38 +0100

    Refactor logger

--- libsysinspect/src/logger.rs
+++ libsysinspect/src/logger.rs
@@ -80,25 +80,27 @@ impl log::Log for STDOUTLogger {
     }
 
     fn log(&self, msg: &Record) {
+        if !self.enabled(msg.metadata()) {
+            return;
+        }
+
         if is_noise_message(&msg.args().to_string()) {
             return;
         }
 
-        if self.enabled(msg.metadata()) {
-            let s_level: String = match msg.level() {
-                log::Level::Info => format!("{}", msg.level().as_str().bright_green()),
-                log::Level::Warn => format!("{}", msg.level().as_str().yellow()),
-                log::Level::Error => format!("{}", msg.level().as_str().bright_red()),
-                log::Level::Debug => format!("{}", msg.level().as_str().bright_cyan()),
-                log::Level::Trace => format!("{}", msg.level().as_str().cyan()),
-            };
+        let s_level: String = match msg.level() {
+            log::Level::Info => format!("{}", msg.level().as_str().bright_green()),
+            log::Level::Warn => format!("{}", msg.level().as_str().yellow()),
+            log::Level::Error => format!("{}", msg.level().as_str().bright_red()),
+            log::Level::Debug => format!("{}", msg.level().as_str().bright_cyan()),
+            log::Level::Trace => format!("{}", msg.level().as_str().cyan()),
+        };
 
-            let mut msg = format!("[{}] - {}: {}", Local::now().format("%d/%m/%Y %H:%M:%S"), s_level, msg.args());
-            if self.nocolor || !std::io::stdout().is_terminal() {
-                msg = strip_ansi_codes(msg.as_str()).into_owned();
-            }
-            println!("{}", msg);
+        let mut msg = format!("[{}] - {}: {}", Local::now().format("%d/%m/%Y %H:%M:%S"), s_level, msg.args());
+        if self.nocolor || !std::io::stdout().is_terminal() {
+            msg = strip_ansi_codes(msg.as_str()).into_owned();
         }
+        println!("{}", msg);
     }
 
     fn flush(&self) {}
@@ -131,14 +133,16 @@ impl log::Log for MemoryLogger {
     }
 
     fn log(&self, record: &Record) {
+        if !self.enabled(record.metadata()) {
+            return;
+        }
+
         if is_noise_message(&record.args().to_string()) {
             return;
         }
 
-        if self.enabled(record.metadata()) {
-            let mut messages = self.messages.lock().unwrap();
-            messages.push(format!("{} - {}", record.level(), record.args()));
-        }
+        let mut messages = self.messages.lock().unwrap();
+        messages.push(format!("{} - {}", record.level(), record.args()));
     }
 
     fn flush(&self) {}
